23741c4f446710ceae5a2a9530842be3f848b69a
akhil <akhilr37@gmail.com>
2022-03-05 10:38:58 +0000

    react code written

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="container">
+    <h1 className="fontsizeing">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="images"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="images"
+      />
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
